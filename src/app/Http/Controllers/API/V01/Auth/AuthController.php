c148c9e2dce85341af424079e8b16d1926108acc
morteza_rahmani <mor98rah@gmail.com>
2020-11-16 15:06:10 +0330

    finish auth

--- src/app/Http/Controllers/API/V01/Auth/AuthController.php
+++ src/app/Http/Controllers/API/V01/Auth/AuthController.php
@@ -4,15 +4,20 @@ namespace App\Http\Controllers\API\V01\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Repositories\UserRepository;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     /**
      * Register New User
-     * @method Post
+     * @method POST
      * @param Request $request
+     * @return JsonResponse
      */
     public function register(Request $request)
     {
@@ -24,23 +29,50 @@ class AuthController extends Controller
         ]);
 
         // Insert User Into Database
-        User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        resolve(UserRepository::class)->create($request);
 
         return response()->json([
-            'message' => 'User created successfully'
+            'message' => 'user created successfully.'
         ], 201);
     }
+
+    /**
+     * Login User
+     * @method GET
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     public function login(Request $request)
     {
+        // Validate Form Inputs
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
 
+        // Check User Credentials For Login
+        if (Auth::attempt($request->only(['email', 'password']))) {
+            return response()->json(Auth::user(), 200);
+        }
+
+        throw ValidationException::withMessages([
+            'email' => "incorrect credentials."
+        ]);
     }
+
+    public function user()
+    {
+        return response()->json(Auth::user(), 200);
+    }
+
     public function logout()
     {
+        Auth::logout();
 
+        return response()->json([
+            'message' => "logged out successfully."
+        ], 200);
     }
 
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -15,10 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+Route::prefix('/v1')->group(function () {
 
-Route::prefix('v1/auth')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
+    // Authentication Routes
+    Route::prefix('/auth')->group(function () {
+        Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+        Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+        Route::get('/user', [AuthController::class, 'user'])->name('auth.user');
+        Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    });
 });
